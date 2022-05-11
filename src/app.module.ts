2e4b5d6652853644a705b65c4c41ce99810220be
ryota-09 <n.ryo950911@gmail.com>
2022-05-11 09:32:53 +0900

    環境変数の設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,13 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://ryota:pSElGbHPNzDkvurP@react-progate-db.npynx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    ),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_ACCESS_CODE),
     AuthModule,
   ],
   controllers: [AppController],
